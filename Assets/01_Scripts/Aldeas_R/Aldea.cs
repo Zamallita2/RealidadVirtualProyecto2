93386350ec7ddb2523b29197e712574f464d9bbd
Robert1532 <apr0034341@est.univalle.edu>
2026-05-26 11:39:44 -0400

    actualizacion de de script alda,testvillage,villagemanager

--- Assets/01_Scripts/Aldeas_R/Aldea.cs
+++ Assets/01_Scripts/Aldeas_R/Aldea.cs
@@ -12,9 +12,141 @@ public enum TipoAldea
 
 public class Aldea : MonoBehaviour
 {
+    [Header("Datos")]
     public TipoAldea tipoAldea;
 
-    public List<AventureroData>
-    aventureros =
-    new List<AventureroData>();
+    [Header("Personajes Base")]
+    public List<AventureroData> plantillaAventureros =
+        new List<AventureroData>();
+
+
+    [Header("Aventureros Actuales")]
+    public List<AventureroData> aventureros =
+        new List<AventureroData>();
+
+
+    [Header("Estado")]
+    public int vidas = 3;
+
+    public int victorias = 0;
+
+
+    void Awake()
+    {
+        if (aventureros.Count == 0)
+        {
+            GenerarNuevoEquipo();
+        }
+    }
+
+
+    //Genera equipo inicial
+    public void GenerarNuevoEquipo()
+    {
+        aventureros.Clear();
+
+
+        List<AventureroData>
+        disponibles =
+        new List<AventureroData>(
+            plantillaAventureros
+        );
+
+
+        while (
+            aventureros.Count < 4 &&
+            disponibles.Count > 0
+        )
+        {
+            int random =
+            Random.Range(
+                0,
+                disponibles.Count
+            );
+
+            AventureroData original =
+            disponibles[random];
+
+
+            AventureroData nuevo =
+            new AventureroData();
+
+            nuevo.nombre =
+            original.nombre;
+
+            nuevo.rol =
+            original.rol;
+
+            nuevo.prefab =
+            original.prefab;
+
+            nuevo.nivel = 1;
+
+
+            aventureros.Add(
+                nuevo
+            );
+
+            disponibles.RemoveAt(
+                random
+            );
+        }
+
+        Debug.Log(
+            tipoAldea +
+            " generó equipo nuevo"
+        );
+    }
+
+
+    //Actualizar personajes
+    public void ActualizarEquipo(
+        List<AventureroData>
+        nuevosDatos,
+        bool ganaron
+    )
+    {
+        aventureros =
+        nuevosDatos;
+
+
+        if (ganaron)
+        {
+            victorias++;
+        }
+
+        Debug.Log(
+            tipoAldea +
+            " actualizó equipo"
+        );
+    }
+
+
+    //Todos murieron
+    public void TodosMurieron()
+    {
+        aventureros.Clear();
+
+        vidas--;
+
+        Debug.Log(
+            tipoAldea +
+            " perdió equipo"
+        );
+
+
+        if (
+            vidas > 0
+        )
+        {
+            GenerarNuevoEquipo();
+        }
+        else
+        {
+            Debug.Log(
+                tipoAldea +
+                " se quedó sin vidas"
+            );
+        }
+    }
 }
--- Assets/01_Scripts/Aldeas_R/VillageManager.cs
+++ Assets/01_Scripts/Aldeas_R/VillageManager.cs
@@ -1,78 +1,18 @@
 using UnityEngine;
-using System.Collections.Generic;
 
 public class VillageManager : MonoBehaviour
 {
-    public List<Aldea>
-    aldeas =
-    new List<Aldea>();
+    public Aldea[] aldeas;
 
 
-    public List<AventureroData>
-    GenerarGrupo()
+    public Aldea ObtenerAldeaAleatoria()
     {
-        List<AventureroData>
-        grupo =
-        new List<AventureroData>();
-
-
-        if (aldeas.Count == 0)
-        {
-            return grupo;
-        }
-
-
-        int randomAldea =
+        int random =
         Random.Range(
             0,
-            aldeas.Count
+            aldeas.Length
         );
 
-
-        Aldea aldeaSeleccionada =
-        aldeas[randomAldea];
-
-
-        Debug.Log(
-            "Aldea seleccionada: "
-            + aldeaSeleccionada.tipoAldea
-        );
-
-
-        List<AventureroData>
-        disponibles =
-        new List<AventureroData>(
-            aldeaSeleccionada
-            .aventureros
-        );
-
-
-        while (
-            grupo.Count < 4 &&
-            disponibles.Count > 0
-        )
-        {
-            int random =
-            Random.Range(
-                0,
-                disponibles.Count
-            );
-
-            AventureroData npc =
-            disponibles[random];
-
-            npc.nivel = 1;
-
-            grupo.Add(
-                npc
-            );
-
-            disponibles.RemoveAt(
-                random
-            );
-        }
-
-
-        return grupo;
+        return aldeas[random];
     }
 }
