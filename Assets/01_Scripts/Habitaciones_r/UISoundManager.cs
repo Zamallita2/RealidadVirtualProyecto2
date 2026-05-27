f8074abd3b365c704a1bcba1a0a0260a3f03b8c8
Robert1532 <apr0034341@est.univalle.edu>
2026-05-27 14:30:52 -0400

    sonido aumentados

--- Assets/01_Scripts/Habitaciones_r/UISoundManager.cs
+++ Assets/01_Scripts/Habitaciones_r/UISoundManager.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class UISoundManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static UISoundManager Instance;
+
+    [Header("Audio Source")]
+    public AudioSource audioSource;
+
+    [Header("Sonidos")]
+    public AudioClip roomLightSound;
+
+    public AudioClip roomButtonSound;
+
+    public AudioClip uiButtonSound;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayRoomLightSound()
     {
-        
+        if (roomLightSound != null)
+        {
+            audioSource.PlayOneShot(roomLightSound);
+        }
+    }
+
+    public void PlayRoomButtonSound()
+    {
+        if (roomButtonSound != null)
+        {
+            audioSource.PlayOneShot(roomButtonSound);
+        }
+    }
+
+    public void PlayUIButtonSound()
+    {
+        if (uiButtonSound != null)
+        {
+            audioSource.PlayOneShot(uiButtonSound);
+        }
     }
 }
